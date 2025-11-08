ec0d1f5ce15aa068770195dcdd35f9c583a5e95e
Kyloof <jankubow.sr@gmail.com>
2025-11-08 16:48:51 +0100

    .

---
+++ nvim1/init.lua
@@ -0,0 +1,7 @@
+require 'kyloof.core.globals'
+require 'kyloof.core.options'
+require 'kyloof.core.keymaps'
+require 'kyloof.core.autocommands'
+require 'kyloof.core.lazy'
+
+vim.cmd.colorscheme 'melange'
